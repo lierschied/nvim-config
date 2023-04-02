46398a4b1faedb757e28ae75b8ee8e99920878a6
lierschied <spielejannis@web.de>
2023-04-02 19:33:44 +0200

    adjusted lsp keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,14 @@
 vim.keymap.set('n', '<leader>w', ':update<CR>', {})
 vim.keymap.set('n', '<leader>Q', ':q<CR>', {})
 
+vim.keymap.set('n', '<leader>L', ':Lazy<CR>', {})
+vim.keymap.set('n', '<leader>li', ':LspInfo<CR>', {})
+vim.keymap.set('n', '<leader>lm', ':Mason<CR>', {})
+
+
+-- Buffers
+vim.keymap.set('n', '<leader>bc', ':bdelete<CR>', {})
+vim.keymap.set('n', '<leader>bh', ':bprevious<CR>', {})
+vim.keymap.set('n', '<leader>bl', ':bnext<CR>', {})
+
 vim.keymap.set('n', '<leader>e', ':Explore<CR>', {})
